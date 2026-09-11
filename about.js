82dcf899e2bbfb22586e4e89fd71c10dfb31689b
Amina Yakdhan <yakdamina29@gmail.com>
2026-09-11 22:42:49 +0100

    Add files via upload

---
+++ about.js
@@ -0,0 +1,18 @@
+export default function About() {
+  return (
+    <div className="page-shell">
+      <section className="card">
+      <h1>Hi, I'm Amina 👋</h1>
+
+      <p>
+        I am a web developer interested in creating
+        modern websites.
+      </p>
+
+      <h2>My Skills : </h2>
+
+      <p>HTML - CSS - JavaScript - React - Next.js</p>
+      </section>
+    </div>
+  );
+}
